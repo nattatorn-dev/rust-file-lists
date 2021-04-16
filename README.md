7afee25d284a8faffe87e49039fe58576bab7f2f
Nattatorn Yucharoen <nattatorn.yuch@Nattatorns-MacBook-Pro.local>
2021-04-16 19:59:33 +0700

    core

---
+++ README.md
@@ -0,0 +1,13 @@
+# File Lists
+
+## Run
+
+```bash
+$ rustc main.rs && ./main
+```
+
+### Regx file name matcher
+
+```bash
+^[A-Z]{3,5}-[0-9]{3,5}$
+```
---
+++ main.rs
@@ -0,0 +1,28 @@
+use std::fs;
+use std::path::Path;
+
+fn list_files(path: &Path) -> () {
+  match fs::read_dir(path) {
+    Err(why) => println!("! {:?}", why.kind()),
+    Ok(paths) => for path in paths {
+        println!("> {:?}", path.unwrap().path());
+    },
+  }
+}
+
+fn main() {
+    let mut path_url_input = String::new();
+    println!("Enter Folder Path (default current path):");
+
+    std::io::stdin().read_line(&mut path_url_input).unwrap();
+
+    let mut path_url = path_url_input.trim().to_string();
+
+    let path_is_empty = path_url.is_empty();
+    if path_is_empty == true {
+      path_url = ".".to_string();
+    }
+
+    let path = &Path::new(&path_url);
+    list_files(path);
+}
